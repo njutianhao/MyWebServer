d2dacacdc0473c9282598d9669aee8b5b945be8f
njutianhao <505187916@qq.com>
2021-02-23 16:11:50 +0800

    update event_loop

--- include/server.h
+++ include/server.h
@@ -4,11 +4,22 @@
 #include<netinet/in.h>
 #include<string.h>
 #include<assert.h>
+#include<sys/epoll.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<stdio.h>
+#define MAX_EVENT_NUM 10000
 class Server{
 private:
     int port;
+    const int backlog = 128;
+    const int epfdsize = 128;
+    int listenfd;
+    int epfd;
+    epoll_event event[MAX_EVENT_NUM];
 public:
     Server();
     void start_listen();
+    void event_loop();
 };
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -1,10 +1,26 @@
 #include "server.h"
+int setnonblocking(int fd) 
+{ 
+    int old_opt = fcntl(fd,F_GETFL);
+    fcntl(fd,old_opt|O_NONBLOCK);
+    return old_opt;
+}
+
+void addfd(int epfd, int fd)
+{ 
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN|EPOLLET;
+    epoll_ctl(epfd,EPOLL_CTL_ADD,fd,&event);
+    setnonblocking(fd);
+}
+
 Server::Server(){
     
 }
 
 void Server::start_listen(){
-    int listenfd = socket(PF_INET,SOCK_STREAM,0);
+    listenfd = socket(PF_INET,SOCK_STREAM,0);
     assert(listenfd >= 0);
     sockaddr_in addr;
     bzero(&addr,sizeof(addr));
@@ -15,4 +31,40 @@ void Server::start_listen(){
     assert(ret >= 0);
     ret = listen(listenfd,backlog);
     assert(ret >= 0);
+    epfd = epoll_create(epfdsize);
+    assert(epfd != -1);
+    addfd(epfd,listenfd);
+}
+
+void Server::event_loop(){
+
+    while(1)
+    {
+        // TODO: TIMEOUT
+        int ret = epoll_wait(epfd,event,MAX_EVENT_NUM,-1);
+        if(ret == -1)
+        {
+            perror("Error");
+            return;
+        }
+        for(int i = 0;i < ret;i++)
+        {
+            if(event[i].data.fd == listenfd)
+            {
+                //TODO:
+            }
+            else if(event[i].events & (EPOLLHUP | EPOLLRDHUP | EPOLLERR))
+            {
+                //TODO:
+            }
+            else if(event[i].events & EPOLLIN)
+            {
+                //TODO:
+            }
+            else if(event[i].events & EPOLLOUT)
+            {
+                //TODO:
+            }
+        }
+    }
 }
